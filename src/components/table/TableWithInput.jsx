4fc75061f289851f99f6b8237a91c3a209610374
Shreyasi Patil <shreyasipatil404@gmail.com>
2025-09-22 11:28:02 +0530

    feat: deselect orders func

--- src/components/table/TableWithInput.jsx
+++ src/components/table/TableWithInput.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { orderColumns } from "../../config/orderTableConfig";
 import TableItem from "./TableItem";
 import styles from "./table.module.css";
+import useOrders from "../../hooks/useOrders";
 
 const TableWithInput = ({ orders, isLoading, limit = 10 }) => {
   const renderRows = isLoading ? Array.from({ length: limit }) : orders;
+  const { selectedOrders, resetOrders } = useOrders();
 
   return (
     <div className={styles.tableContainer}>
@@ -12,7 +14,13 @@ const TableWithInput = ({ orders, isLoading, limit = 10 }) => {
         <thead>
           <tr>
             <th>
-              <input type="checkbox" disabled />
+              <input
+                type="checkbox"
+                checked={selectedOrders.length > 0}
+                onClick={() => {
+                  resetOrders();
+                }}
+              />
             </th>
             {orderColumns.map((header, idx) => (
               <th key={idx}>{header} </th>
